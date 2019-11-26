422f99bf3b302201f42962e02413c07cec8478b4
safronman <safronmanbox@gmail.com>
2019-11-26 16:24:46 +0300

    24. Регистрация локализации #registerLocaleData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, Provider} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -12,6 +14,7 @@ import {SharedModule} from './admin/shared.module';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthInterceptor} from './shared/auth.interceptor';
 
+registerLocaleData(ruLocale, 'ru');
 
 export const INTERCEPTOR_PROVIDER: Provider = {
     provide: HTTP_INTERCEPTORS,
