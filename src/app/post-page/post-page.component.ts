98d5c66fca14b9e637663b31cfe5b08725f56e93
safronman <safronmanbox@gmail.com>
2019-11-28 14:41:28 +0300

    26. Отображение поста #activatedroute #async pipe #switchMap #route.params

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
+import {Post} from '../admin/shared/interfaces/interfaces';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-post-page',
@@ -7,10 +12,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-    constructor() {
+    post$: Observable<Post>;
+
+    constructor(
+        private route: ActivatedRoute,
+        private postsService: PostsService
+    ) {
     }
 
     ngOnInit() {
+        this.post$ = this.route.params
+            .pipe(
+                // позволяет изменить направление стрима от params до нужного нам стрима Хм...
+                switchMap((params: Params) => {
+                    return this.postsService.getPostById(params.id);
+                })
+            );
     }
 
 }
